09081e648b1c22a64b6eeea91edc070570d0724d
kramber1024 <kramber268@gmail.com>
2024-07-19 23:19:14 +0300

    feat: Add proxy fix

--- app/main.py
+++ app/main.py
@@ -1,9 +1,17 @@
 from flask import Flask
+from werkzeug.middleware.proxy_fix import ProxyFix
 
 from app.config import settings
 from app.views import blueprint
 
 app: Flask = Flask(__name__)
+app.wsgi_app = ProxyFix(  # type: ignore[method-assign]
+    app.wsgi_app,
+    x_for=1,
+    x_proto=1,
+    x_host=1,
+    x_prefix=1,
+)
 app.register_blueprint(blueprint)
 
 if __name__ == "__main__":
