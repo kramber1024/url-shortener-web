f26b37876be5385c7db5c378ff5b268aea103791
kramber1024 <kramber268@gmail.com>
2024-08-24 16:49:20 +0300

    feat: Add Layout component

---
+++ src/components/Layout/Layout.tsx
@@ -0,0 +1,18 @@
+import type { ReactElement, ReactNode } from "react";
+import styles from "./Layout.module.scss";
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout = ({ children }: LayoutProps): ReactElement => {
+  return (
+    <html lang="en">
+      <body>
+        <div className={styles.fullpage}>{children}</div>
+      </body>
+    </html>
+  );
+};
+
+export default Layout;
