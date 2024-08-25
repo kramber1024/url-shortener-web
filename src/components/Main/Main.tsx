7b63a06c440ba23f73be73dc4ed5edf199a56238
kramber1024 <kramber268@gmail.com>
2024-08-25 18:25:05 +0300

    feat: Add props

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import type React from "react";
+import type { ReactNode } from "react";
 
 import styles from "./Main.module.scss";
 
-export const Main = (): React.JSX.Element => {
-  return <main className={styles.content}></main>;
+interface MainProps {
+  children: ReactNode;
+}
+
+export const Main = ({ children }: Readonly<MainProps>): React.JSX.Element => {
+  return <main className={styles.content}>{children}</main>;
 };
