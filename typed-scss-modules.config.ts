b319f09fb516a3550e94a39f139b4585e8ce0d5e
kramber1024 <kramber268@gmail.com>
2024-08-25 22:10:12 +0300

    feat: Add config for scss types

---
+++ typed-scss-modules.config.ts
@@ -0,0 +1,10 @@
+export const config = {
+  watch: true,
+  nameFormat: "camel",
+  exportType: "named",
+  quoteType: "double",
+  aliasPrefixes: {
+    "~styles": "./src/styles",
+  },
+  ignore: ["node_modules"],
+};
