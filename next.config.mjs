15c3965711c14c9c3f8f57c1ec4d63d5e08eb922
kramber1024 <kramber268@gmail.com>
2024-08-25 21:02:04 +0300

    feat: Resolve path alias for sass

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,17 @@
+import path from "path";
+import { config } from "process";
+import { fileURLToPath } from "url";
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.alias["@styles"] = "src/styles";
+
+    return config;
+  },
+};
 
 export default nextConfig;
