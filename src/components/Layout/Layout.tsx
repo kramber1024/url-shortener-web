e5182e439c1413d07c9bbbff102a6b4078157b1e
kramber1024 <kramber268@gmail.com>
2024-08-25 18:24:25 +0300

    feat: Add viewport meta

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import "@styles/global.scss";
 
+import type { Viewport } from "next";
 import { Inter } from "next/font/google";
 import type React from "react";
 import type { ReactNode } from "react";
@@ -7,6 +8,13 @@ import type { ReactNode } from "react";
 import styles from "./Layout.module.scss";
 
 const inter = Inter({ subsets: ["latin", "cyrillic"], weight: ["400", "500", "600"] });
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  minimumScale: 1,
+  maximumScale: 5,
+};
 
 interface LayoutProps {
   children: ReactNode;
