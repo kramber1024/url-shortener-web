f08e048c56073629faa344c0c67806a01c4dd1f2
kramber1024 <kramber268@gmail.com>
2024-08-26 14:33:06 +0300

    feat: Update AuthForm component

--- src/components/layouts/AuthForm/AuthForm.module.scss.d.ts
+++ src/components/layouts/AuthForm/AuthForm.module.scss.d.ts
@@ -1,2 +1,1 @@
 export declare const authForm: string;
-export declare const authFormFooter: string;
--- src/components/layouts/AuthForm/AuthForm.tsx
+++ src/components/layouts/AuthForm/AuthForm.tsx
@@ -5,26 +5,19 @@ import styles from "./AuthForm.module.scss";
 
 interface AuthFormProps {
   title: string;
-  footer: string;
   children: ReactNode;
 }
 
 export const AuthForm = ({
   title,
-  footer,
   children,
 }: Readonly<AuthFormProps>): React.JSX.Element => {
   return (
-    <>
-      <div className={styles.authForm}>
-        <h1>{title}</h1>
-        <form noValidate={true} target="_self">
-          {children}
-        </form>
-      </div>
-      <div className={styles.authFormFooter}>
-        <p>{footer}</p>
-      </div>
-    </>
+    <div className={styles.authForm}>
+      <h1>{title}</h1>
+      <form noValidate={true} target="_self">
+        {children}
+      </form>
+    </div>
   );
 };
