353b68430e368cfb6fce3744212224b05957d54e
kramber1024 <kramber268@gmail.com>
2024-08-26 22:36:51 +0300

    refactor: Drop css modules

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,15 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes("_app")) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    });
     config.resolve.alias["@styles"] = "src/styles/";
     config.resolve.alias["@components"] = "src/components/";
 
