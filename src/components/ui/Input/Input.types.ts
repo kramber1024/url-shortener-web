a14279c86c1471096edb91e824f493e92e896aeb
kramber1024 <kramber268@gmail.com>
2024-08-27 00:20:25 +0300

    refactor: Add .types

---
+++ src/components/ui/Input/Input.types.ts
@@ -0,0 +1,21 @@
+import type { ChangeEvent } from "react";
+
+type Autocomplete =
+  | "current-password"
+  | "email"
+  | "family-name"
+  | "given-name"
+  | "new-password"
+  | "off";
+
+type InputType = "email" | "password" | "text";
+
+export interface InputProps {
+  autocomplete?: Autocomplete;
+  name: string;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  placeholder?: string;
+  required?: boolean;
+  title: string;
+  type?: InputType;
+}
