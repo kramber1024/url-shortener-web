10e6e8d72b9bb7f343050dc7f426026cef313e59
kramber1024 <kramber268@gmail.com>
2024-08-26 23:45:12 +0300

    feat: Add onSubmit prop

--- src/components/layouts/AuthForm/AuthForm.tsx
+++ src/components/layouts/AuthForm/AuthForm.tsx
@@ -7,12 +7,13 @@ import "./AuthForm.scss";
 export const AuthForm = ({
   children,
   name = "auth-form",
+  onSubmit,
   title,
 }: Readonly<AuthFormProps>): React.JSX.Element => {
   return (
     <div className="auth-form">
       <h1 className="auth-form-title">{title}</h1>
-      <form id={name} name={name} noValidate={true} target="_self">
+      <form id={name} name={name} noValidate={true} onSubmit={onSubmit} target="_self">
         {children}
       </form>
     </div>
--- src/components/layouts/AuthForm/AuthForm.types.ts
+++ src/components/layouts/AuthForm/AuthForm.types.ts
@@ -1,7 +1,8 @@
-import type { ReactNode } from "react";
+import type { FormEvent, ReactNode } from "react";
 
 export interface AuthFormProps {
   children: ReactNode;
   name?: string;
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
   title: string;
 }
