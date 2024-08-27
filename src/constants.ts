cba8e543cd3d73c330012822628699c6efbe3040
kramber1024 <kramber268@gmail.com>
2024-08-27 19:03:37 +0300

    feat: Add constants

---
+++ src/constants.ts
@@ -0,0 +1,21 @@
+interface Constants {
+  EMAIL_MAX_LENGTH: number;
+  EMAIL_MIN_LENGTH: number;
+  FIRST_NAME_MAX_LENGTH: number;
+  FIRST_NAME_MIN_LENGTH: number;
+  LAST_NAME_MAX_LENGTH: number;
+  LAST_NAME_MIN_LENGTH: number;
+  PASSWORD_MAX_LENGTH: number;
+  PASSWORD_MIN_LENGTH: number;
+}
+
+export const Constants: Constants = {
+  EMAIL_MAX_LENGTH: 64,
+  EMAIL_MIN_LENGTH: "a@b.c".length,
+  FIRST_NAME_MAX_LENGTH: 16,
+  FIRST_NAME_MIN_LENGTH: 3,
+  LAST_NAME_MAX_LENGTH: 16,
+  LAST_NAME_MIN_LENGTH: 3,
+  PASSWORD_MAX_LENGTH: 256,
+  PASSWORD_MIN_LENGTH: 8,
+} as const;
