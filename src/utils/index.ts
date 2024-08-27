6c3ace51cd8e838c968ab113261485c36df64a61
kramber1024 <kramber268@gmail.com>
2024-08-27 19:03:51 +0300

    feat: Add utils for working with strings

---
+++ src/utils/index.ts
@@ -0,0 +1,1 @@
+export { toTitle } from "./string";
---
+++ src/utils/string.ts
@@ -0,0 +1,6 @@
+export const toTitle = (str: string): string => {
+  if (!str) {
+    return str;
+  }
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
