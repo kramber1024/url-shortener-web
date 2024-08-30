7cfd9aae5d7d0beac325b60c11ea7a6fef8470f7
kramber1024 <kramber268@gmail.com>
2024-08-30 15:52:18 +0300

    feat: Add status codes to each response type

--- src/types/interfaces/responses.ts
+++ src/types/interfaces/responses.ts
@@ -1,17 +1,27 @@
-interface Message {
-  message: string;
+/**
+ * Status codes that api can return
+ */
+type HTTPSuccessStatusCode = 200 | 201;
+type HTTPClientErrorStatusCode = 400 | 401 | 404 | 409 | 422;
+type HTTPServerErrorStatusCode = 500;
+
+interface BaseResponse {
+  readonly message: string;
+  readonly status: number;
 }
 
-export interface Error extends Message {
-  type: string;
+export interface SuccessResponse extends BaseResponse {
+  readonly status: HTTPSuccessStatusCode;
 }
 
-export interface SuccessResponse extends Message {
-  status: number;
+export interface Error {
+  readonly message: string;
+  readonly type: string;
 }
 
-export interface ErrorResponse extends SuccessResponse {
-  errors: Error[];
+export interface ErrorResponse extends BaseResponse {
+  readonly errors: Error[];
+  readonly status: HTTPClientErrorStatusCode | HTTPServerErrorStatusCode;
 }
 
 export type ApiResponse = ErrorResponse | SuccessResponse;
