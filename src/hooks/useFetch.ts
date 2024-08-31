42a119f5fa329cb0ecb1eba203db40c33f927543
kramber1024 <kramber268@gmail.com>
2024-08-31 14:19:28 +0300

    feat: Add callback to hook

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -7,23 +7,30 @@ const headers: Record<string, string> = { "Content-Type": "application/json" };
 export const useFetch = <T extends object>(
   method: RestMethod,
   url: string
-): [boolean, null | T, (body: unknown) => void] => {
-  const [data, setData] = useState<null | T>(null);
+): [boolean, (body: unknown, callback?: (data: null | T) => void) => void] => {
   const [loading, setLoading] = useState<boolean>(false);
 
   const fetchData = useCallback(
-    async (body: unknown) => {
+    (body: unknown, callback?: (data: null | T) => void): void => {
       setLoading(true);
-      const response: Response = await fetch(url, {
+      fetch(url, {
         body: JSON.stringify(body),
         headers,
         method,
-      });
-      setData(response.json() as T);
-      setLoading(false);
+      }).then(
+        async (response: Response): Promise<void> => {
+          const data = (await response.json()) as T;
+          setLoading(false);
+          callback?.(data);
+        },
+        () => {
+          setLoading(false);
+          callback?.(null);
+        }
+      );
     },
     [method, url]
   );
 
-  return [loading, data, fetchData];
+  return [loading, fetchData];
 };
