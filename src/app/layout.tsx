d8d9110f40fb243eff928b3b0fb750628f0451c3
kramber <kramber268@gmail.com>
2024-09-02 10:18:15 +0300

    refactor: Remove root layout component

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,29 @@
+import type { Viewport } from "next";
 import type { ReactNode } from "react";
 
-import { Layout } from "@/components/layouts/Layout";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin", "cyrillic"], weight: ["400", "500", "600"] });
+
+export const viewport: Viewport = {
+  initialScale: 1,
+  maximumScale: 5,
+  minimumScale: 1,
+  width: "device-width",
+};
 
 interface RootLayoutProps {
   children: ReactNode;
 }
 
 const RootLayout = ({ children }: Readonly<RootLayoutProps>) => {
-  return <Layout>{children}</Layout>;
+  return (
+    <html lang="en">
+      <body>
+        <div className={`fullpage ${inter.className}`}>{children}</div>
+      </body>
+    </html>
+  );
 };
 
 export default RootLayout;
