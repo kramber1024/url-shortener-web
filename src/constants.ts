9e1bab9ecee355e86b1edf4d4a98484539ed77d0
kramber <kramber268@gmail.com>
2024-09-03 14:55:54 +0300

    feat: Add consts for hosting

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,15 @@
+type Scheme = "http" | "https";
+
+const parseScheme = (scheme: string | undefined): Scheme => {
+  if (!scheme || (scheme !== "http" && scheme !== "https")) {
+    return "http";
+  }
+  return scheme;
+};
+
+const scheme = parseScheme(process.env.SCHEME);
+const host = process.env.HOST ?? "127.0.0.1";
+
 interface App {
   NAME: string;
 }
@@ -13,10 +25,19 @@ interface Data {
   PASSWORD_MIN_LENGTH: number;
 }
 
+interface Host {
+  DOMAIN: string;
+  HOST: string;
+  PORT: number;
+  SCHEME: Scheme;
+  URL: `${Scheme}://${string}`;
+}
+
 interface Constants {
   /** App related constants. E.g ` NAME ` for whole app. */
   app: App;
   data: Data;
+  host: Host;
 }
 
 export const constants: Constants = {
@@ -32,5 +53,12 @@ export const constants: Constants = {
     LAST_NAME_MIN_LENGTH: 3,
     PASSWORD_MAX_LENGTH: 256,
     PASSWORD_MIN_LENGTH: 8,
+  },
+  host: {
+    DOMAIN: process.env.DOMAIN ?? "ushort.com",
+    HOST: host,
+    PORT: parseInt(process.env.PORT ?? "26802", 10),
+    SCHEME: scheme,
+    URL: `${scheme}://${host}`,
   },
 } as const;
