73de878a479398ab797fa449914342b1702b9f6e
kramber1024 <kramber268@gmail.com>
2024-09-04 23:23:12 +0300

    feat: Minor changes

---
+++ src/components/styles/css/units.ts
@@ -0,0 +1,14 @@
+export type AbsoluteUnit = "cm" | "in" | "mm" | "pc" | "pt" | "px" | "Q";
+
+export type RelativeUnit =
+  | "%"
+  | "ch"
+  | "em"
+  | "ex"
+  | "lh"
+  | "rem"
+  | "svh"
+  | "vh"
+  | "vmax"
+  | "vmin"
+  | "vw";
