ede7a50707b0e2cedfad05db3a405e9879bee3fe
kramber <kramber268@gmail.com>
2024-09-09 10:42:48 +0300

    feat: Remove aliases

--- next.config.mjs
+++ next.config.mjs
@@ -1,18 +1,6 @@
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  webpack: (config) => {
-    /** Aliases for sass */
-    config.resolve.alias["@/styles"] = "src/styles/";
-    config.resolve.alias["@/components"] = "src/components/";
-
-    return config;
-  },
 };
 
 export default nextConfig;
