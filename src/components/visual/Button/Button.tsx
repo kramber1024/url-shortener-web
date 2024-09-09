75f48558c54c7b66efdc3574492b19e0effc6154
kramber <kramber268@gmail.com>
2024-09-09 17:35:13 +0300

    feat: Update Button

--- src/components/visual/Button/Button.tsx
+++ src/components/visual/Button/Button.tsx
@@ -13,25 +13,14 @@ export const Button = ({
   type = "button",
 }: Readonly<ButtonProps>): React.JSX.Element => {
   return (
-    <>
-      <style jsx>
-        {`
-          @media (max-width: 700px) {
-            ${button} {
-              background-color: red;
-            }
-          }
-        `}
-      </style>
-      <button
-        className={button}
-        disabled={disabled}
-        form={form}
-        onClick={onClick}
-        type={type}
-      >
-        {loading ? <div className={loader} /> : children}
-      </button>
-    </>
+    <button
+      className={button}
+      disabled={disabled}
+      form={form}
+      onClick={onClick}
+      type={type}
+    >
+      {loading ? <div className={loader} /> : children}
+    </button>
   );
 };
--- src/components/visual/index.ts
+++ src/components/visual/index.ts
@@ -1,2 +1,4 @@
 export { Brand } from "./Brand";
 export type { BrandProps } from "./Brand";
+export { Button } from "./Button";
+export type { ButtonProps } from "./Button/Button.types";
