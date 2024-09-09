0fd8768d72de1c22cb4a9c7dcaa7bb8cb2bec22b
kramber <kramber268@gmail.com>
2024-09-09 14:13:57 +0300

    feat: Add wrapper component

---
+++ src/components/layout/index.ts
@@ -0,0 +1,8 @@
+export { Fullpage } from "./Fullpage";
+export type { FullpageProps } from "./Fullpage";
+
+export { Main } from "./Main";
+export type { MainProps } from "./Main";
+
+export { Wrapper } from "./Wrapper";
+export type { WrapperProps } from "./Wrapper";
