a515938f1b22dc9fa2a5fb3621f517d76b27ed7c
kramber <kramber268@gmail.com>
2024-09-09 14:46:40 +0300

    feat: Update components used

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,13 +1,12 @@
 import type React from "react";
 import type { ReactNode } from "react";
 
-import { Main } from "@/components/layouts/Main";
-import { Navbar } from "@/components/layouts/Navbar";
-import { Brand } from "@/components/ui/Brand";
+import { Main } from "@/components/layout";
+import { Navbar } from "@/components/navigation";
+import { Brand } from "@/components/visual";
 import { constants } from "@/constants";
-import Image from "next/image";
 
-export interface AuthLayoutProps {
+interface AuthLayoutProps {
   children: ReactNode;
 }
 
@@ -15,16 +14,11 @@ const AuthLayout = ({ children }: Readonly<AuthLayoutProps>): React.JSX.Element 
   return (
     <>
       <Navbar>
-        <Brand name={constants.app.NAME}>
-          <Image
-            alt={`${constants.app.NAME} brand logo`}
-            height={32}
-            src="/svg/logo.svg"
-            width={32}
-          />
-        </Brand>
+        <Brand name={constants.app.NAME} />
       </Navbar>
-      <Main>{children}</Main>
+      <Main alignItems="center" justifyContent="center">
+        {children}
+      </Main>
     </>
   );
 };
