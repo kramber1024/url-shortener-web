d992b2f3aa7a92560d9ec0ec71bc3714750882ea
kramber <kramber268@gmail.com>
2024-09-12 10:29:47 +0300

    feat: Add "Forgot password?" link to passwrod type FormInput

--- src/components/form/FormInput/FormInput.tsx
+++ src/components/form/FormInput/FormInput.tsx
@@ -2,6 +2,8 @@ import type React from "react";
 
 import type { FormInputProps } from "./FormInput.types";
 
+import { Box } from "../../layout";
+import { Link } from "../../navigation";
 import { Label } from "../../typography";
 import { Input } from "../Input";
 import { formInput } from "./FormInput.module.scss";
@@ -15,7 +17,19 @@ export const FormInput = ({
 }: Readonly<FormInputProps>): React.JSX.Element => {
   return (
     <div className={formInput}>
-      <Label htmlFor={name}>{titleMap[type]}</Label>
+      {type === "password" ? (
+        <Box
+          display="flex"
+          flexDirection="row"
+          justifyContent="space-between"
+          width="100%"
+        >
+          <Label htmlFor={name}>{titleMap[type]}</Label>
+          <Link href="#">Forgot password?</Link>
+        </Box>
+      ) : (
+        <Label htmlFor={name}>{titleMap[type]}</Label>
+      )}
       <Input
         id={name}
         name={name}
