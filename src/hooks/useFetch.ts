b6f33be842b393d96c21613fbabc3b25fdd8e332
kramber <kramber268@gmail.com>
2024-09-16 11:26:26 +0300

    doc: Add documentation for custom hook refactor: Minor changes

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,11 +1,30 @@
 import { useCallback, useState } from "react";
 
-type RestMethod = "DELETE" | "GET" | "POST" | "PUT";
+const headers: Record<string, string> = { "Content-Type": "application/json" } as const;
 
-const headers: Record<string, string> = { "Content-Type": "application/json" };
-
+/**
+ * Fetch data from the server.
+ *
+ * This hook is made in a way that client trusts the server
+ * and does not validate server response.
+ * @example
+ * const [loading, fetchData] = useFetch<{message: string}>("GET", "https://api.example.com/data");
+ * fetchData({}, (data) => {
+ *  if (data) {
+ *    console.log(data.message);
+ *  } else {
+ *    console.error("Failed to fetch data");
+ *   }
+ * });
+ * @template T - Expected response data type.
+ * @param method - HTTP method.
+ * @param url - URL to fetch data from. All subsequent requests will be made to this URL.
+ * @returns
+ * - loading - Indicates whether the request is in progress. Can be used for loading indicators.
+ * - fetchData - Function with callback to fetch data from the server.
+ */
 export const useFetch = <T extends object>(
-  method: RestMethod,
+  method: "DELETE" | "GET" | "POST" | "PUT",
   url: string
 ): [boolean, (body: unknown, callback?: (data: null | T) => void) => void] => {
   const [loading, setLoading] = useState<boolean>(false);
