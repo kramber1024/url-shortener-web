cce040a58efbb80e476cf9e96275997e78658139
kramber <kramber268@gmail.com>
2024-09-24 11:22:38 +0300

    chore: Update ignored files

--- jest.config.ts
+++ jest.config.ts
@@ -8,6 +8,12 @@ const createJestConfig = nextJest({
 const config: Config = {
   coverageProvider: "v8",
   collectCoverageFrom: ["src/**/*.{ts,tsx}"],
+  coveragePathIgnorePatterns: [
+    "src/.*\\.module\\.(?:css|scss)\\.d\\.ts",
+    "src/.*index\\.ts",
+    "src/.*\\.cy.(?:ts|tsx)",
+    "cypress/.*",
+  ],
 };
 
 export default createJestConfig(config);
