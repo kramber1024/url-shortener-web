e5661ebb71b7a94fbafd9566b806e863d5961cb5
kramber <kramber268@gmail.com>
2024-09-26 13:17:01 +0300

    refactor: Separate validateForm onto multiple functions

--- src/app/(auth)/login/validation.ts
+++ src/app/(auth)/login/validation.ts
@@ -4,34 +4,70 @@ import { constant } from "@/constant";
 import { errorInvalid, errorRequired } from "@/utils";
 
 /**
+ * Validate the email by checking if it is empty, too short, too long, or invalid.
+ * Shows only the required and invalid errors.
+ * @param email - The email to validate.
+ * @param minLength - The minimum length of the email.
+ * @param maxLength - The maximum length of the email.
+ * @returns An array of errors.
+ */
+export const validateEmail = (
+  email: string,
+  minLength: number,
+  maxLength: number
+): Error[] => {
+  const validationErrors: Error[] = [];
+  const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
+  if (!email) {
+    validationErrors.push(errorRequired("email"));
+  } else if (
+    !emailRegex.test(email) ||
+    email.length < minLength ||
+    email.length > maxLength
+  ) {
+    validationErrors.push(errorInvalid("email"));
+  }
+  return validationErrors;
+};
+
+/**
+ * Validate the password by checking if it is empty, too short, or too long.
+ * Shows only the required and invalid errors.
+ * @param password - The password to validate.
+ * @param minLength - The minimum length of the password.
+ * @param maxLength - The maximum length of the password.
+ * @returns An array of errors.
+ */
+export const validatePassword = (
+  password: string,
+  minLength: number,
+  maxLength: number
+): Error[] => {
+  const validationErrors: Error[] = [];
+  if (!password) {
+    validationErrors.push(errorRequired("password"));
+  } else if (password.length < minLength || password.length > maxLength) {
+    validationErrors.push(errorInvalid("password"));
+  }
+  return validationErrors;
+};
+
+/**
  * Client-side validate the login form
  * @param formData - The form data to validate.
  * @returns An array of errors.
  */
 export const validateForm = (formData: LoginUser): Error[] => {
-  const validationErrors: Error[] = [];
-
-  /* Validate email */
-  const emailRegex = new RegExp(/^[^\s@]+@[^\s@]+\.[^\s@]+$/);
-  if (!formData.email) {
-    validationErrors.push(errorRequired("email"));
-  } else if (
-    !emailRegex.test(formData.email) ||
-    formData.email.length < constant.data.EMAIL_MIN_LENGTH ||
-    formData.email.length > constant.data.EMAIL_MAX_LENGTH
-  ) {
-    validationErrors.push(errorInvalid("email"));
-  }
-
-  /* Validate password */
-  if (!formData.password) {
-    validationErrors.push(errorRequired("password"));
-  } else if (
-    formData.password.length < constant.data.PASSWORD_MIN_LENGTH ||
-    formData.password.length > constant.data.PASSWORD_MAX_LENGTH
-  ) {
-    validationErrors.push(errorInvalid("password"));
-  }
-
-  return validationErrors;
+  return [
+    ...validateEmail(
+      formData.email,
+      constant.data.EMAIL_MIN_LENGTH,
+      constant.data.EMAIL_MAX_LENGTH
+    ),
+    ...validatePassword(
+      formData.password,
+      constant.data.PASSWORD_MIN_LENGTH,
+      constant.data.PASSWORD_MAX_LENGTH
+    ),
+  ];
 };
