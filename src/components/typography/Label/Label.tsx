0956b1dbcc18591cff0e2f37383ca49ee20b0525
kramber <kramber268@gmail.com>
2024-09-28 14:31:01 +0300

    feat: Add attribute for testing

--- src/components/typography/Label/Label.tsx
+++ src/components/typography/Label/Label.tsx
@@ -7,11 +7,13 @@ import { black, green, red, text } from "./Label.module.scss";
 export const Label = ({
   children,
   color = 1,
+  dataTestId,
   htmlFor,
 }: Readonly<LabelProps>): React.JSX.Element => {
   return (
     <label
       className={`${text} ${[black, red, green].at(color - 1) ?? ""}`}
+      data-testid={dataTestId}
       htmlFor={htmlFor}
     >
       {children}
--- src/components/typography/Label/Label.types.ts
+++ src/components/typography/Label/Label.types.ts
@@ -3,5 +3,6 @@ import type { ReactNode } from "react";
 export interface LabelProps {
   children: ReactNode;
   color?: 1 | 2 | 3;
+  dataTestId?: string;
   htmlFor: string;
 }
